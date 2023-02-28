a8651c85871316cef6b0ea81dd21739b14dfd343
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-02-28 21:57:09 +0300

    Initial commit

---
+++ src/test/java/com/example/my_final_project/MyFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
