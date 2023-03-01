e44ca1cddbe9e573e83d78e99e6bd01e8f6de252
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-03-01 20:12:08 +0300

    First Controller

--- src/main/java/com/example/my_final_project/MyFinalProjectApplication.java
+++ src/main/java/com/example/my_final_project/MyFinalProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyFinalProjectApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(MyFinalProjectApplication.class, args);
     }
 
